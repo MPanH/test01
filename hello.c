988c5b6bc984a6f56ce80cc78775acdbb9866096
phh <phh@xdja.com>
2015-06-18 14:09:15 +0800

    test

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+
+{
+  printf("231231"); 
+ printf("2333333333"); 
+  printf("231231231\n"); 
+   printf("this is a test!\n");
+    printf("11111222222222!\n");
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+hello:hello.o
+	gcc -o hello hello11.o
+hello.o:
+	gcc -c hello.c
+
